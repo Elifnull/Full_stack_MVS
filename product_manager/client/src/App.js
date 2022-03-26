83aec9b129b8c22300a29a77f6038fc9dd2cb87a
Serge Chvorun <elifnull@protonmail.com>
2022-03-25 23:46:14 -0400

    updated product manager

--- product_manager/client/src/App.js
+++ product_manager/client/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import ProductForm from './components/ProductForm';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <ProductForm/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ProductForm/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ product_manager/client/src/components/ProductForm.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+
+const ProductForm = (props) => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState(0);
+
+    const onSubmitHandler = (e) => {
+
+        e.preventDefault();
+
+        axios.post("http://localhost:8000/api/product",{
+            title,
+            price,
+            description
+        })
+            .then(result => {
+                console.log(result);
+                console.log(result.body);
+                setTitle("");
+                setDescription("");
+                setPrice("");
+            })
+            .catch(err => console.log(err))
+    }
+
+
+    return(
+        <div>
+            <h2>Product Manager</h2>
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={(e)=> setTitle(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input type="number" value={price} onChange={e => setPrice(e.target.valueAsNumber)}/>
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input type="text" value={description} onChange={(e)=> setDescription(e.target.value)}/>
+                </div>
+                <input type="submit" value="Create"/>
+            </form>
+        </div>
+    )
+}
+
+export default ProductForm;
