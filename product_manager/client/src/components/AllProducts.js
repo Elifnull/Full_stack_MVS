2f0d71f0937628265689ee633f7465678353a550
Serge Chvorun <elifnull@protonmail.com>
2022-03-26 03:03:52 -0400

    updated backend

---
+++ product_manager/client/src/components/AllProducts.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const AllProducts = (props) => {
+    const [productArray, setProductArray] = useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/product')
+            .then(result =>{
+                console.log(result);
+                console.log(result.data);
+                setProductArray(result.data);
+            })
+            .catch(err => console.log(err))
+    },[])
+
+
+    return(
+        <div>
+            <header>
+                <Link to={"/"}>Create Product</Link>
+            </header>
+            {
+                productArray.map((product,index)=>(
+                    <div key={index}>
+                        <p>Name of Product: {product.title}</p>
+                        <p>Description: {product.description}</p>
+                        <p>Price: ${product.price}</p>
+                        <br/>!!!
+                    </div>
+                ))
+            }
+        </div>
+    )
+
+}
+
+export default AllProducts;
