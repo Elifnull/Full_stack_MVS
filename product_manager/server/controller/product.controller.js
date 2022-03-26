79137f7bd3bf2136b1f04e17b6b84383f9cca0e2
Serge Chvorun <elifnull@protonmail.com>
2022-03-26 02:26:53 -0400

    updated product with CRUD backend

--- product_manager/server/controller/product.controller.js
+++ product_manager/server/controller/product.controller.js
@@ -3,5 +3,30 @@ const Product = require('../model/product.model');
 module.exports.createProduct = (request, response) => {
     Product.create(request.body)
         .then(createdProduct => response.json(createdProduct))
-        .catch(err => response.json(err));
+        .catch(err => response.status(400).json(err));
+}
+module.exports.findAllProducts = (request, response) => {
+    Product.find({})
+        .then(foundProducts=> {
+            console.log(foundProducts);
+            response.json(foundProducts);
+        })
+        .catch(err => res.json({message: `error ${err}`}, err));
+}
+
+module.exports.findOneProduct = (request, response) => {
+    Product.findById({_id:request.params.id})
+        .then(productById => {
+            console.log(productById)
+            response.json({product:productById});}
+        )
+        .catch(err =>{
+
+        });
+}
+
+module.exports.deleteOneProduct = (response, request) => {
+    Product.deleteOne({_id: request.params.id})
+        .then(uponDelete => res.json(uponDelete))
+        .catch(err=> response.json({message: `error:${err}`}));
 }
